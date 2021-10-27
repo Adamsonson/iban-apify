fb456321c88094eb007cacfdc3ce020c21be9d62
Adamsonson <adamsomora33@gmail.com>
2021-10-27 21:46:44 +0200

    handle active recodrs errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_destroyed(data)
+    render json: { errors: data.record.errors }, status: :unprocessable_entity
+  end
+
+  def not_found
+    render json: { errors: 'Unable to find IBAN' }, status: :not_found
+  end
 end
